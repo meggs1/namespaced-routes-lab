9f722223ed1dc4f384c9e4140c6d26c6c41d8824
megan <megan.schertz@gmail.com>
2021-03-23 15:05:36 -0500

    commit tree 75b241a928dd33a220176e6741209eb865f60d54

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126235547) do
+ActiveRecord::Schema.define(version: 20210323195003) do
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.boolean  "allow_create_artists"
+    t.boolean  "allow_create_songs"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "songs", force: :cascade do |t|
